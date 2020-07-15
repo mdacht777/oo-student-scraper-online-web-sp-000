7d11832452404adc64440144b6436054c68ef6f4
Martin Achtermann <mdacht777@gmail.com>
2020-07-15 15:00:03 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -31,7 +31,7 @@ class CommandLineInterface
       student.instance_variables.each do |attr, value|
         # binding.pry
         label=attr
-        attr.gsub(/\@/, "")
+        attr=attr.gsub(/\@/, "")
         puts " #{attr}:  #{student.instance_variable_get(attr)}" if student.instance_variable_get(attr)!=nil
       end
       # puts "  location:".colorize(:light_blue) + " #{student.location}"
